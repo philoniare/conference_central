49615263e412438a5a57ac90264021b2cc88108a
philoniare <philoniare@gmail.com>
2016-01-31 19:15:09 -0500

    Added description of the project and application configurations

---
+++ README.md
@@ -0,0 +1,5 @@
+# Overview
+Conference API endpoints in Python built to run a Google App Engine
+
+# License
+Released under the [MIT License](http://opensource.org/licenses/MIT).
---
+++ helloworld_api.py
@@ -0,0 +1,10 @@
+"""Hello World API using Google Cloud Endpoints.
+
+Contains endpoint declarations, methods, as well as
+ProtoRPC class and container for endpoint method definition
+"""
+
+import endpoints
+from protorpc import messages
+from protorpc import message_types
+from protorpc import remote
